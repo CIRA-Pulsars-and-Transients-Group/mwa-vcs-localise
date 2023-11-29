20eee37aca06bc03dc4625b92d956ca40d846685
Bradley Meyers <bradley.meyers1993@gmail.com>
2023-11-29 19:11:47 +0800

    skeleton utils

---
+++ mwa-vcs-localise/utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+########################################################
+# Licensed under the Academic Free License version 3.0 #
+########################################################
+
+import typing
+from attrs import define, field
+import astropy.units as u
+from astropy.io import fits
+
+# Define some type aliases for convenience
+intvec: typing.TypeAlias = list[int]
+fltvec: typing.TypeAlias = list[float]
+strvec: typing.TypeAlias = list[str]
+
+
+@define
+class Array:
+    array_config: str
+    array_centre_hasl: u.Quantity = 377.827 * u.m
+    antenna_id: intvec
+    tile_names: strvec
+    tile_ids: list
+    tile_eastings: fltvec  # tile easting from array centre
+    tile_northings: fltvec  # tile northing from array centre
+    tile_heights: fltvec  # tile height coordinate ABOVE SEA LEVEL
