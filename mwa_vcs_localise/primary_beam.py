3fd9fb270cd554165d64ec282d57407a827e8f4d
Bradley Meyers <bradley.meyers1993@gmail.com>
2023-12-05 11:31:22 +0800

    removed additional primary beam function, made previous function multi-dimensional

--- mwa_vcs_localise/primary_beam.py
+++ mwa_vcs_localise/primary_beam.py
@@ -22,44 +22,13 @@ def getPrimaryBeamPower(
     sky = np.eye(2)
 
     jones = beam.calc_jones_array(
-        np.array([az]),
-        np.array([za]),
+        np.array([az]).flatten(),
+        np.array([za]).flatten(),
         freq_hz,
         metadata.delays,
         np.ones_like(metadata.delays),
         zenithNorm,
     )
-    jmat = np.matrix(jones.reshape((2, 2)))
-    power = np.real(np.trace(jmat @ sky @ jmat.H))
-    return power
-
-
-def getPrimaryBeamPower_2D(
-    metadata: MetafitsContext,
-    freq_hz: float,
-    alt0: float,
-    alt1: float,
-    az0: float,
-    az1: float,
-    ncells: int,
-    zenithNorm: bool = True,
-):
-    za0 = np.pi / 2 - alt0
-    za1 = np.pi / 2 - alt1
-    az, za = make_grid(az0, az1, za0, za1, ncells)
-
-    beam = PrimaryBeam()
-    sky = np.eye(2)
-
-    jones = beam.calc_jones_array(
-        az.flatten(),
-        za.flatten(),
-        freq_hz,
-        metadata.delays,
-        np.ones_like(metadata.delays),
-        zenithNorm,
-    )
-    jmat = np.matrix(jones.reshape((2, 2)))
-    power = np.real(np.trace(jmat @ sky @ jmat.H))
-
-    return az, za, power.reshape(az.shape)
+    jmats = [np.matrix(j.reshape((2, 2))) for j in jones]
+    power = [np.real(np.trace(jmat @ sky @ jmat.H)) for jmat in jmats]
+    return np.array(power)
