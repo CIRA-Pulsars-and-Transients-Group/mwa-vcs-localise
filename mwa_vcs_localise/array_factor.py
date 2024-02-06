a3ef55daa0b5333017f40130a9e2b83fdb3d4ead
Bradley Meyers <bradley.meyers1993@gmail.com>
2024-02-06 14:05:58 +0800

    use broadcasting and tensordots to compute phasors more efficiently, include function to just extract tile positions (reduce repeated actions)

--- mwa_vcs_localise/array_factor.py
+++ mwa_vcs_localise/array_factor.py
@@ -11,26 +11,88 @@ from astropy.constants import c as sol
 from .utils import MWA_CENTRE_CABLE_LEN
 
 
-def calcGeometricDelays(
-    metadata: MetafitsContext, freq_hz: float, alt: float, az: float
-):
-    unit_N = np.cos(alt) * np.cos(az)
-    unit_E = np.cos(alt) * np.sin(az)
-    unit_H = np.sin(alt)
+def extractWorkingTilePositions(metadata: MetafitsContext):
+    """Extract tile position information required for beamforming and/or
+    computing the array factor quantity from a metafits structure.
+    Flagged tiles are automatically excluded from the result.
+
+    :param metadata: An MWALIB MetafitsContext structure
+                     containing the array layout information.
+    :type metadata: MetafitsContext
+    :return: Working tile positions and electrical lengths for
+             beamforming. Formatted as an array of arrays, where
+             each item in the outer array is:
+                [east_m, north_m, height_m, electrical_length_m]
+             for a single tile.
+    :rtype: np.ndarray
+    """
+    # We only care about the tiles, not polarisations, so skip the Y pol
     rf_inputs = metadata.rf_inputs[::2]
-    phi = []
-    for rf in rf_inputs:
-        e = rf.east_m
-        n = rf.north_m
-        h = rf.height_m
-        ell = rf.electrical_length_m - MWA_CENTRE_CABLE_LEN.value
 
-        w = e * unit_E + n * unit_N + h * unit_H
-        dt = (w - ell) / sol.value
-        phase = 2 * np.pi * freq_hz * dt
-        phi.append(np.cos(phase) + 1.0j * np.sin(phase))
+    # Gather the tile positions into a "vector" for each tile
+    tile_positions = np.array(
+        [
+            np.array(
+                [
+                    rf.east_m,
+                    rf.north_m,
+                    rf.height_m,
+                    rf.electrical_length_m - MWA_CENTRE_CABLE_LEN.value,
+                ]
+            )
+            for rf in rf_inputs
+        ]
+    )
 
-    return np.array(phi)
+    # Gather the flagged tile information from the metafits information
+    # and remove those tiles from the above vector
+    tile_flags = np.array([rf.flagged for rf in rf_inputs])
+    tile_positions = np.delete(tile_positions, np.where(tile_flags == True), axis=0)
+
+    return tile_positions
+
+
+def calcGeometricDelays(positions: np.ndarray, freq_hz: float, alt: float, az: float):
+    """Compute the geometric delay phases for each element position in order to
+    "phase up" to the provided position at a specific frequency. These are the
+    phasors used in a beamforming operation.
+
+    :param positions: An array or element position vectors, including their
+                      equivalent electrical length, in metres.
+    :type positions: np.ndarray
+    :param freq_hz: Observing radio frequency, in Hz.
+    :type freq_hz: float
+    :param alt: Desired altitude for the pointing direction, in radians. Can be an array.
+    :type alt: np.ndarray, float
+    :param az: Desired azimuth for the pointing direction, in radians. Can be an array.
+    :type az: np.ndarray, float
+    :return: The required phasors needed to rotate the element patterns to each requested az/alt pair.
+    :rtype: np.ndarray, complex
+    """
+    # Create the unit vector(s)
+    u = np.array(
+        [
+            np.cos(alt) * np.sin(az),  # unit E
+            np.cos(alt) * np.cos(az),  # unit N
+            np.sin(alt),  # unit H
+            -np.ones_like(alt),  # cable length (-ve as it is subtracted)
+        ]
+    )
+
+    # Compute the equivalent delay length for each tile
+    # (Use tensor dot product so we can choose to keep the
+    # dimensionality of the alt/az grid and continue using
+    # broadcasting rules efficiently.)
+    w = np.tensordot(positions, u, axes=1)
+
+    # Convert to a time delay
+    dt = w / sol.value
+
+    # Construct the phasor
+    phase = 2 * np.pi * freq_hz * dt
+    phasor = np.exp(1.0j * phase)
+
+    return phasor
 
 
 def calcArrayFactorPower(look_w, target_w):
@@ -48,6 +110,7 @@ def calcArrayFactorPower(look_w, target_w):
     :rtype: np.ndarray
     """
     # At this stage, the shape of target_w = (nant, n_ra, n_dec) and while the shape of look_w = (nant,)
+    print("... summing over antennas")
     sum_over_antennas = np.tensordot(np.conjugate(look_w), target_w, axes=1)
     # From the numpy.tensordot documentation:
     #    The third argument can be a single non-negative integer_like scalar, N;
@@ -56,5 +119,6 @@ def calcArrayFactorPower(look_w, target_w):
 
     # The array factor power is normalised to the number of elements
     # included in the sum (i.e., length of the `look_w` vector).
+    print("... converting to power")
     afp = (np.absolute(sum_over_antennas) / look_w.size) ** 2
     return afp
