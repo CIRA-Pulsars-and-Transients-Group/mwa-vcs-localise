94b615b5b69da486145c0362b7ed7eb3c8b46442
Bradley Meyers <bradley.meyers1993@gmail.com>
2024-04-17 11:30:35 +0800

    removed deprecated/unused fns

--- mwa_vcs_localise/utils.py
+++ mwa_vcs_localise/utils.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import ConvexHull
 from scipy.spatial.distance import cdist
-from astropy.coordinates import EarthLocation, Angle, SkyCoord, concatenate
+from astropy.coordinates import EarthLocation
 import astropy.units as u
 from mwalib import MetafitsContext
 
@@ -22,81 +22,6 @@ MWA_CENTRE_CABLE_LEN = 0.0 * u.m
 MWA_LOCATION = EarthLocation.from_geodetic(
     lon=MWA_CENTRE_LON, lat=MWA_CENTRE_LAT, height=MWA_CENTRE_H
 )
-
-
-def create_layer(args) -> SkyCoord:
-    layer_idx, ncore, sym_ang, width, central_coord = args
-    sfact = layer_idx + 1
-    nnodes_this_layer = sfact * ncore
-    nodes = np.empty(nnodes_this_layer, dtype=SkyCoord)
-    for node_idx in range(nnodes_this_layer):
-        new_node = create_node(sfact, node_idx, sym_ang, width, central_coord)
-        nodes[node_idx] = new_node
-    return nodes
-
-
-def create_node(scale, node_idx, sym_ang, width, central_coord) -> SkyCoord:
-    pa = node_idx * sym_ang / scale
-    r = width * scale
-    return central_coord.directional_offset_by(pa, r)
-
-
-def form_grid_positions(
-    central_coords: SkyCoord,
-    max_separation_arcsec: float = 60.0,
-    nlayers: int = 1,
-    overlap: bool = False,
-    ncpus: int = None,
-) -> SkyCoord:
-    """Based on a central sky position, tile beams around it
-    with a given separation.
-
-    :param central_coords: The central beam positions, around which to
-        tile beams.
-    :type central_coords: SkyCoord
-    :param max_separation_arcsec: Maximum radial separation (away from
-        central point), defaults to 60.0
-    :type max_separation_arcsec: float, optional
-    :param nlayers: Number of layers of beams to produce, defaults to 1
-    :type nlayers: int, optional
-    :param overlap: Whether to allow some overlap of "fwhm" radius
-        (effectively shrinks max. separation by ~80%), defaults to False
-    :type overlap: bool, optional
-    :param verbose: Output extra information, defaults to False
-    :type verbose: bool, optional
-    :param ncpus: Number of processes to use when computing sky nodes,
-        defaults to None (use all CPUs)
-    :type ncpus: int, optional
-    :return: A SkyCoord object containing all produced tiling beams.
-    :rtype: SkyCoord
-    """
-    nbeams_core = 6
-    symmetry_angle = Angle(360 * u.deg / nbeams_core)
-    fwhm = Angle(max_separation_arcsec * u.arcsecond)
-
-    if overlap:
-        rho = np.sqrt(3) / 2.0  # optimal coverage for hexagonal circle packing, N=7
-    else:
-        rho = 1
-
-    # sum((j + 1) * (nbeams - 1) for j in range(nlayers)) can be written
-    # as the following, given that range(N) = 0, 1, ..., n-1
-    total_nodes = int(nlayers * (nlayers + 1) * nbeams_core / 2)
-    print(f"nlayers = {nlayers}, total nodes = {total_nodes}")
-
-    pkgs = []
-    for j in range(nlayers):
-        pkgs.append((j, nbeams_core, symmetry_angle, rho * fwhm, central_coords))
-
-    with multiprocessing.Pool(processes=ncpus) as pool:
-        results = pool.map(create_layer, pkgs)
-
-    # results is a list of numpy arrays, where each element is a SkyCoord object
-    nodes = np.concatenate(results)
-    nodes = np.append(nodes, central_coords)
-    print("finished creating nodes")
-
-    return concatenate(nodes)
 
 
 def find_max_baseline(context: MetafitsContext) -> list:
@@ -150,10 +75,12 @@ def plot_array_layout(context: MetafitsContext) -> None:
     bad_tiles_n = np.ma.masked_array(tile_positions[:, 1], mask=~tile_flags)
     bad_tiles_e = np.ma.masked_array(tile_positions[:, 0], mask=~tile_flags)
 
-    fig = plt.figure(figsize=plt.figaspect(1))
+    fig = plt.figure()
     fig.add_subplot()
     plt.scatter(okay_tiles_e, okay_tiles_n, zorder=1000, s=10, marker="x", color="k")
     plt.scatter(bad_tiles_e, bad_tiles_n, zorder=1000, s=10, marker="x", color="r")
+    plt.xlim(-410, 410)
+    plt.ylim(50, 600)
     plt.xlabel("East coordinate from array centre (m)", fontsize=14)
     plt.ylabel("North coordiante from array centre (m)", fontsize=14)
     plt.title(
@@ -166,21 +93,3 @@ def plot_array_layout(context: MetafitsContext) -> None:
     plt.grid(which="minor", ls=":")
     plt.savefig(f"{context.obs_id}_array_layout.png", dpi=200, bbox_inches="tight")
     plt.close(fig)
-
-
-def makeMaskFromWeightedPatterns(
-    tab_collection: list,
-    snr_collection: list,
-    tab_threshold: float = 0.1,
-    snr_percentile: int = 95,
-):
-    clip_mask = np.zeros_like(tab_collection[0])
-    ntab = len(tab_collection)
-    for i, tab in enumerate(tab_collection):
-        tab_mask = np.zeros_like(tab)
-        tab_mask[tab >= tab_threshold] = snr_collection[i]
-        clip_mask += tab_mask
-    clip_mask[clip_mask < 2] = np.nan
-    clip_mask[clip_mask < np.nanpercentile(clip_mask, snr_percentile)] = np.nan
-
-    return clip_mask
