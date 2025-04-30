293ee5f1097d332eeedc16875c86702615353e74
Bradley Meyers <bradley.meyers1993@gmail.com>
2025-04-30 11:48:58 +0800

    moved debug plotting function to utils, not used in normal execution

--- src/mwa_vcs_localise/utils.py
+++ src/mwa_vcs_localise/utils.py
@@ -6,6 +6,7 @@
 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
+import matplotlib.colors as colors
 import cmasher as cm
 
 import numpy as np
@@ -297,3 +298,80 @@ def plot_tied_array_beam(
         oname_base += oname_suffix
 
     plt.savefig(f"{oname_base}.png", dpi=200, bbox_inches="tight")
+
+
+def __plot_tab_centres_and_contours(
+    beam_cen_coords,
+    tabp,
+    grid_ra,
+    grid_dec,
+    label,
+    contours=True,
+) -> None:
+    """
+    Making a plot of the beam and contours for looks, with the beam
+    centres marked. Mostly for debugging.
+    """
+
+    tabp_sum = np.sum(tabp, axis=0)
+
+    map_extent = [grid_ra.min(), grid_ra.max(), grid_dec.min(), grid_dec.max()]
+
+    aspect = "equal"
+
+    cmap = cm.get_sub_cmap(cm.cosmic, 0.1, 0.9)
+    cmap.set_bad("red")
+    contour_cmap = cm.get_sub_cmap(cm.cosmic_r, 0.1, 0.9)
+    cmapnorm_sum = colors.Normalize(vmin=1e-5, vmax=0.1, clip=True)
+    cmapnorm_indiv = colors.Normalize(vmin=1e-5, vmax=0.05, clip=True)
+
+    fig = plt.figure(figsize=(10, 10))
+    ax1 = fig.add_subplot(1, 1, 1)
+    ax1_img = ax1.imshow(
+        tabp_sum, aspect=aspect, extent=map_extent, cmap=cmap, norm=cmapnorm_sum
+    )
+
+    ax1.plot(
+        beam_cen_coords.ra.deg,
+        beam_cen_coords.dec.deg,
+        "Dy",
+        mec="k",
+        ms=5,
+        label="Beam centers",
+    )
+
+    if contours:
+        for ls, look in enumerate(tabp):
+            ax1.contour(
+                look,
+                origin="image",
+                extent=map_extent,
+                cmap=contour_cmap,
+                norm=cmapnorm_indiv,
+                linewidths=0.5,
+            )
+
+    ax1.legend(fontsize=18, loc=2)
+    ax1.set_xlabel("R.A. (ICRS)", fontsize=18, ha="center")
+    ax1.set_ylabel("Dec. (ICRS)", fontsize=18, ha="center")
+    ax1.minorticks_on()
+    ax1.tick_params(axis="both", which="major", labelsize=18)
+    ax1.tick_params(axis="both", which="major", length=9)
+    ax1.tick_params(axis="both", which="minor", length=4.5)
+    ax1.tick_params(axis="both", which="both", direction="out", right=True, top=True)
+
+    cbar = fig.colorbar(
+        ax1_img,
+        ax=fig.axes,
+        shrink=1,
+        orientation="horizontal",
+        location="top",
+        aspect=30,
+        pad=0.02,
+    )
+    cbar.ax.set_title(label, fontsize=18, ha="center")
+    cbar.ax.xaxis.set_ticks_position("top")
+    cbar.ax.tick_params(direction="in", length=5, bottom=True, top=True)
+    cbar.ax.xaxis.set_tick_params(labelsize=18)
+
+    plt.savefig("tabs_with_centres.png", bbox_inches="tight")
